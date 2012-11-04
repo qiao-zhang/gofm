2ecded8a5569b4d2ea1357e898a8bfe87dc641cd
linju <linju@douban.com>
2012-11-04 12:20:56 +0800

    basic command line iteractive env

--- src/cli/main.go
+++ src/cli/main.go
@@ -2,14 +2,79 @@ package main
 
 import (
     "fmt"
-    "model"
+    "time"
+    "os"
 )
 
+func PrintHelp(){
+    fmt.Println(`Gofm - A Douban.fm Command Line Interface
+
+Github: 
+    http://dou.bz/3c8G7L
+    等你的pull request ;)
+
+Useage:
+    h, help: list help
+    q, quit: quit Gofm
+    f, fav: fav the song playing now
+    u, unfav: unfav the song playing now
+    d, del: move the song playing now into trash
+    l, loop: loop playing current song
+    s, skip: skip the song playing now
+    r, rec: show recommand channel
+    ls, list: show all channel
+    c %, choose %: change channel, type id or name both ok. example:
+        >> l
+        华语(1) 欧美(2)
+        >> c 1
+        >> c 华语`)
+}
+
+func PrintNotSupport() {
+    fmt.Println(`not supported cmd ;(`)
+}
 
 func main() {
-    c := &model.Creator{"doubanfm", "http://douban.fm"}
-    fmt.Println( c )
+    ch := make(chan string)
+    PrintHelp()
+    fmt.Print(">> ")
 
-    ch := model.NewChannel("Huayu", "doubanfm", 1024, c)
-    fmt.Println( ch )
+    go func() {
+        fmt.Println("\rAlways Online - 林俊杰 00:11\b2")
+        time.Sleep(1e9 * 10)
+    }()
+
+    go func() {
+        for {
+            cmd := <-ch
+            switch cmd {
+                case "h":
+                    fallthrough
+                case "help":
+                    PrintHelp()
+                case "q":
+                    fallthrough
+                case "quit":
+                    os.Exit(0)
+                case "":
+                    // do nothing
+                default:
+                    PrintNotSupport()
+            }
+            fmt.Print(">> ")
+        }
+    }()
+
+    go func() {
+        var cmd string
+        for {
+            fmt.Scanln(&cmd)
+            ch <- cmd
+            cmd = ""
+        }
+    }()
+
+    for {
+        time.Sleep( time.Second )
+    }
 }
